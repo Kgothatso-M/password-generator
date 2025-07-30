fba16b81c5cca8cb5b318adb106c74159b85483b
Kgothatso-M <kgothatsomagopane21@gmail.com>
2025-07-30 12:51:39 +0200

    Added password generator

---
+++ script.js
@@ -0,0 +1,11 @@
+const generateBtn = document.getElementById('generate');
+const passwordField = document.getElementById('password');
+
+generateBtn.addEventListener('click', () => {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*';
+  let password = '';
+  for (let i = 0; i < 12; i++) {
+    password += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  passwordField.value = password;
+});
